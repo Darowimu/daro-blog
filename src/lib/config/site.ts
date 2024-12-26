29f7deb271c3a0f423821374e1109b436f73f170
Darowimu <115894674+Darowimu@users.noreply.github.com>
2024-12-26 22:37:54 +0100

    Update site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -3,15 +3,15 @@ import type { SiteConfig } from '$lib/types/site'
 export const site: SiteConfig = {
   author: {
     avatar: '/assets/maskable@512.png',
-    bio: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    name: 'John Doe',
+    bio: 'Some random person that decided to blog.',
+    name: 'Darowimu',
     status: '🌸',
   },
   description: 'Powered by SvelteKit/Urara',
   domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
   lang: 'en-US',
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
-  subtitle: 'Sweet & Powerful SvelteKit Blog Template',
+  subtitle: '',
   themeColor: '#3D4451',
-  title: 'Urara',
+  title: 'Daro Blog',
 }
